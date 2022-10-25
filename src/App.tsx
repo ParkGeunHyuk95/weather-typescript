040633c0472c85d8460424a8c4d12e4043194e3e
parkgeunhyuk1 <108388408+parkgeunhyuk1@users.noreply.github.com>
2022-10-25 17:13:46 +0900

    typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
 import './App.css';
+import WeatherBox from './components/WeatherBox';
+import WeatherButton from './components/WeatherButton';
 
 function App() {
+  const [weather, setWeather] = useState<object>();
+  const cities:string[] = ["MOSCOW", "SEOUL", "NEW YORK", "PARIS"];
+  const [hello,setHello]=useState<string>('안녕')
+  const [city, setCity] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const getCurrentLocation = ():void => {
+    setLoading(true);
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat:number = position.coords.latitude;
+      let lon:number = position.coords.longitude;
+      getWeatherByCurrentLocation(lat, lon);
+      setLoading(false);
+    });
+  };
+  const getWeatherByCity = async ():Promise<void> => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e5a8c13b789b8a02ee32533666cff55f&units=metric`;
+    setLoading(true);
+    let res = await fetch(url);
+    let data = await res.json();
+    setWeather(data);
+    setLoading(false);
+  };
+  const getWeatherByCurrentLocation = async (lat:number, lon:number) :Promise<void>=> {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e5a8c13b789b8a02ee32533666cff55f&units=metric`;
+    setLoading(true);
+    let res = await fetch(url);
+    let data:object = await res.json();
+    setWeather(data);
+    setLoading(false);
+  };
+  useEffect(() => {
+    if (city == "") {
+      getCurrentLocation();
+    } else {
+      getWeatherByCity();
+    }
+  }, [city]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading ? (
+        <div className="Container">
+          <ClipLoader color="#f88c6b" loading={loading} size={150} />
+        </div>
+      ) : (
+        <div className="Container">
+          <WeatherBox weather={weather} />
+          <WeatherButton
+            cities={cities}
+            city={city}
+            setCity={setCity}
+            getCurrentLocation={getCurrentLocation}
+          />
+        </div>
+      )}
+    </>
   );
 }
 
